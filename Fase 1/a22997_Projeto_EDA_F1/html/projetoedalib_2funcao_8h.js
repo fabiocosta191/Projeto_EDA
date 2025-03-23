722904fcce5e6ebe803cff4fda06e5665d094709
Fábio Costa <132665477+fabiocosta191@users.noreply.github.com>
2025-03-23 15:04:01 +0000

    Add files via upload

---
+++ Fase 1/a22997_Projeto_EDA_F1/html/projetoedalib_2funcao_8h.js
@@ -0,0 +1,19 @@
+var projetoedalib_2funcao_8h =
+[
+    [ "ANT", "struct_a_n_t.html", "struct_a_n_t" ],
+    [ "NEF", "struct_n_e_f.html", "struct_n_e_f" ],
+    [ "MAX_COLUNAS", "projetoedalib_2funcao_8h.html#ad624bf76d9366faecf9041ef6ffffcb9", null ],
+    [ "MAX_LINHAS", "projetoedalib_2funcao_8h.html#ab44f6d43935e7b204b295865fc8c1edc", null ],
+    [ "ANT", "projetoedalib_2funcao_8h.html#a18e249b9aec3c65c0b80b819088c780a", null ],
+    [ "NEF", "projetoedalib_2funcao_8h.html#a79d00fd6dc572ce81ad082edad8b19b5", null ],
+    [ "apresentarLista", "projetoedalib_2funcao_8h.html#af957e4140e89bdf54056d6d381c35b82", null ],
+    [ "apresentarListaNef", "projetoedalib_2funcao_8h.html#a909731b047c9da8b3fd53e08b9fdefe9", null ],
+    [ "apresentarMatriz", "projetoedalib_2funcao_8h.html#ac31478cd4b125ff169d04263a3ba9573", null ],
+    [ "atualizarMatriz", "projetoedalib_2funcao_8h.html#a45d5ccc3ee9341474afc44e73475d76f", null ],
+    [ "igualarMatrizes", "projetoedalib_2funcao_8h.html#a79802d5e95d26d76bca730e07ce288e8", null ],
+    [ "inserirAntena", "projetoedalib_2funcao_8h.html#a0caf0b54deb90e026d66c8ee5d718c6a", null ],
+    [ "inserirNefasto", "projetoedalib_2funcao_8h.html#a60d611b34c7be07e4e578482ac3749ef", null ],
+    [ "LerLista", "projetoedalib_2funcao_8h.html#a0dd3787db1a292cee40267e850469704", null ],
+    [ "matrizNefastos", "projetoedalib_2funcao_8h.html#a7c100c209dce8c29abd13961c75f2fcb", null ],
+    [ "removerAntena", "projetoedalib_2funcao_8h.html#a5e363afa7bf8a0d74560ec1e766a52f6", null ]
+];
---
+++ Fase 1/a22997_Projeto_EDA_F1/html/search/functions_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['main_0',['main',['../main_8c.html#ae66f6b31b5ad750f1fe042a706a4e3d4',1,'main.c']]],
+  ['matriznefastos_1',['matrizNefastos',['../projetoeda_2funcao_8h.html#a7c100c209dce8c29abd13961c75f2fcb',1,'matrizNefastos(char matriz[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas):&#160;funcao.c'],['../funcao_8c.html#a7c100c209dce8c29abd13961c75f2fcb',1,'matrizNefastos(char matriz[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas):&#160;funcao.c'],['../projetoedalib_2funcao_8h.html#a7c100c209dce8c29abd13961c75f2fcb',1,'matrizNefastos(char matriz[MAX_LINHAS][MAX_COLUNAS], int linhas, int colunas):&#160;funcao.c']]]
+];
